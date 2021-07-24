cf09d83a2c18e98176c333b3f05560ee7a74ac1f
haguse <halitguvens@gmail.com>
2021-07-24 23:45:14 +0300

    Get Countries from Covid19Api.com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import covidLogo from "./coronavirus.svg";
+import { fetchCountries } from "./api";
+import AreaChart from "./components/AreaChart";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: "50px auto",
+    width:"50%"
+  },
+}));
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("turkey");
+
+  useEffect(() => {
+    const fetchCountriesData = async () => {
+      const countries = await fetchCountries();
+      setCountries(countries);
+    };
+    fetchCountriesData();
+  }, []);
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container>
+          <img
+            src={covidLogo}
+            alt="Covid19 Logo"
+            style={{ width: "100px", height: "100px", marginTop: "20px" }}
+          ></img>
+
+          <FormControl className={classes.formControl}>
+            <Select
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            >
+              {countries.map((country) => {
+                return (
+                  <MenuItem value={country.Slug}>{country.Country}</MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+
+          {/* Chart */}
+
+          <Grid item xs={12}>
+            <AreaChart />
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
