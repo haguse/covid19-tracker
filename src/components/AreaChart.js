f48c420023861792ee4fc33d0d33da86f2ff8af5
haguse <halitguvens@gmail.com>
2021-07-25 00:36:44 +0300

    Create Apex Chart

--- src/components/AreaChart.js
+++ src/components/AreaChart.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Chart from "react-apexcharts";
+import { fetchDailyData } from "../api";
 
-const AreaChart = () => {
-    return (
-        <div>
-            area
-        </div>
-    )
-}
+const AreaChart = ({ country }) => {
+  const [dailyData, setDailyData] = useState([]);
 
-export default AreaChart
+  useEffect(() => {
+    const fetchCountryDailyData = async () => {
+      const data = await fetchDailyData(country);
+      setDailyData(data);
+    };
+    fetchCountryDailyData();
+  }, [country]);
+
+  return (
+    <div id="chart">
+      <Chart
+        options={{
+          chart: {
+            type: "area",
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            curve: "smooth",
+          },
+          xaxis: {
+            type: "datetime",
+            categories: dailyData.map((item) => item.Date),
+          },
+          tooltip: {
+            x: {
+              format: "dd/MM/yy",
+            },
+          },
+        }}
+        series={[
+          {
+            name: "Cases",
+            data: dailyData.map((item) => item.Confirmed),
+          },
+          {
+            name: "Recovered",
+            data: dailyData.map((item) => item.Recovered),
+          },
+          {
+            name: "Deaths",
+            data: dailyData.map((item) => item.Deaths),
+          },
+        ]}
+        style={{ marginTop: "40px" }}
+        height={350}
+      ></Chart>
+    </div>
+  );
+};
+
+export default AreaChart;
